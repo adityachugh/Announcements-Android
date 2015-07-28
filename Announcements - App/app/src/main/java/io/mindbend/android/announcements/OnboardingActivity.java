82c2db7cbd7f07abb877120f8b0f12e3082aabd9
AvikHasija <avikhasija@gmail.com>
2015-07-28 14:26:20 -0400

    Project Moved to Folder
    in "Announcements - Android"

--- Announcements - App/app/src/main/java/io/mindbend/android/announcements/OnboardingActivity.java
+++ Announcements - App/app/src/main/java/io/mindbend/android/announcements/OnboardingActivity.java
@@ -6,18 +6,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class BoardingActivity extends ActionBarActivity {
+public class OnboardingActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_boarding);
+        setContentView(R.layout.activity_onboarding);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_boarding, menu);
+        getMenuInflater().inflate(R.menu.menu_onboarding, menu);
         return true;
     }
 
