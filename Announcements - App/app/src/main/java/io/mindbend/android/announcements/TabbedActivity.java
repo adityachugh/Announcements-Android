d02fadb2c7023c69cda8edd46def71dc3c657cec
AvikHasija <avikhasija@gmail.com>
2015-07-28 15:53:17 -0400

    Tabbed Activity Added

--- Announcements - App/app/src/main/java/io/mindbend/android/announcements/TabbedActivity.java
+++ Announcements - App/app/src/main/java/io/mindbend/android/announcements/TabbedActivity.java
@@ -2,6 +2,7 @@ package io.mindbend.android.announcements;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,9 @@ public class TabbedActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabbed);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
