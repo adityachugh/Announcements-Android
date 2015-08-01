71581ac62582335d9f257b8156e094149d4c5d02
Akshay Pall <iosakshay@gmail.com>
2015-08-01 14:53:23 -0400

    Added Parse

--- Announcements - App/app/src/main/java/io/mindbend/android/announcements/App.java
+++ Announcements - App/app/src/main/java/io/mindbend/android/announcements/App.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.os.Build;
 import android.util.Log;
 
+import com.parse.Parse;
+
 /**
  * Created by Akshay Pall on 01/08/2015.
  */
@@ -16,6 +18,12 @@ public class App extends Application {
         super.onCreate();
 //        TODO: this class is to initialize parse and setup anything else that is required for the app to run. This class is run FIRST before any activity.
         Log.d(APP_STARTED, "Application started succesfully");
+
+        // Initialize Parse and Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "S1dL5D6QCSqsC1FyYTiyS5V4Yv2zcK47TeybVtEf", "llxS4kjhRdSv46DvGi9HyPapsRaFDoU9155Nh88V");
+        Log.d(APP_STARTED, "Parse initialized");
+
 //        set if the device is running lollipop or higher to decide whether to run animations or not
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             isLollipopOrHigher = true;
