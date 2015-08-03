cd0b400a38b41f4845fbc5e4a7c6116b218bcbef
Akshay Pall <iosakshay@gmail.com>
2015-08-03 15:59:51 -0400

    "Post Comment" Dialog Added
    Dialog to post a comment and select which account to post from NOW
    WORKING. Data not handled right now (of course)

--- Announcements - App/app/src/main/java/io/mindbend/android/announcements/tabbedFragments/TodayFragment.java
+++ Announcements - App/app/src/main/java/io/mindbend/android/announcements/tabbedFragments/TodayFragment.java
@@ -3,18 +3,26 @@ package io.mindbend.android.announcements.tabbedFragments;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.transition.Scene;
 import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -40,6 +48,9 @@ public class TodayFragment extends Fragment implements PostsFeedAdapter.PostInte
     public boolean isOnComments = false;
     private Fragment mCurrentComments;
     private ImageButton mFab;
+
+    //saving the last post in order to get the club's name, as used in the "add comment" dialog
+    Post mLastPost;
 
 
     public TodayFragment() {
@@ -83,6 +94,50 @@ public class TodayFragment extends Fragment implements PostsFeedAdapter.PostInte
     public void onClick(View v) {
         if (isOnComments){
             //TODO: dialogue box to add a comment
+            // get prompts.xml view
+            LayoutInflater li = LayoutInflater.from(getActivity());
+            View addCommentView = li.inflate(R.layout.add_comment_dialog, null);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+
+            // set the dialog's view to alertdialog builder
+            alertDialogBuilder.setView(addCommentView);
+
+            //work with all the elements in the dialog
+            final EditText userInput = (EditText) addCommentView.findViewById(R.id.add_comment_edittext);
+
+            //set the subtext to notify the user on WHOSE post they are commenting on
+            TextView subText = (TextView)addCommentView.findViewById(R.id.add_comment_subtext);
+            subText.setText(getString(R.string.add_comment_dialog_subtext, mLastPost.getmPostClubUsername()));
+
+            //setting up the spinner(dropdown) to select which user/club to post the comment from
+            Spinner spinner = (Spinner)addCommentView.findViewById(R.id.user_spinner);
+            //TODO: dynamically create the user list
+            String[] userArray = {"User 1", "User 2"};
+            ArrayAdapter<String> userAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, userArray);
+            spinner.setAdapter(userAdapter);
+
+            // set dialog message
+            alertDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("Post",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,int id) {
+                                    //TODO: add a comment to the comments post
+                                }
+                            })
+                    .setNegativeButton("Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
+
+            // show it
+            alertDialog.show();
         }
         else {
             //TODO: dialogue box to change today date
@@ -102,6 +157,8 @@ public class TodayFragment extends Fragment implements PostsFeedAdapter.PostInte
 
     @Override
     public void pressedPost(Post postPressed) {
+        mLastPost = postPressed;
+
         isOnComments = true;
 
         //set the fab's src/icon to the "add a new comment" image
