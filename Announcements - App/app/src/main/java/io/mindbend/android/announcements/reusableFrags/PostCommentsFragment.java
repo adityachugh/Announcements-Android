2416027d6ba5b85e9523cf1131c580dd219254e7
Akshay Pall <iosakshay@gmail.com>
2015-08-30 14:51:30 -0400

    load more comments st bottom WORKING

--- Announcements - App/app/src/main/java/io/mindbend/android/announcements/reusableFrags/PostCommentsFragment.java
+++ Announcements - App/app/src/main/java/io/mindbend/android/announcements/reusableFrags/PostCommentsFragment.java
@@ -27,6 +27,8 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.orangegangsters.github.swipyrefreshlayout.library.SwipyRefreshLayout;
+import com.orangegangsters.github.swipyrefreshlayout.library.SwipyRefreshLayoutDirection;
 import com.parse.FunctionCallback;
 import com.parse.ParseException;
 
@@ -40,7 +42,7 @@ import io.mindbend.android.announcements.R;
 import io.mindbend.android.announcements.User;
 import io.mindbend.android.announcements.cloudCode.CommentsDataSource;
 
-public class PostCommentsFragment extends Fragment implements Serializable, PostCommentsAdapter.CommenterInteractionListener, SwipeRefreshLayout.OnRefreshListener {
+public class PostCommentsFragment extends Fragment implements Serializable, PostCommentsAdapter.CommenterInteractionListener, SwipyRefreshLayout.OnRefreshListener {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_POST = "post";
     private static final String ARG_LISTENER = "comment_listener";
@@ -53,7 +55,7 @@ public class PostCommentsFragment extends Fragment implements Serializable, Post
     private transient ImageButton mFab;
     private CommentsInteractionListener mListener;
     private transient View mView;
-    private transient SwipeRefreshLayout mRefreshComments;
+    private transient SwipyRefreshLayout mRefreshComments;
     private transient RecyclerView mRecyclerView;
     private transient RelativeLayout mLoading;
 
@@ -61,7 +63,7 @@ public class PostCommentsFragment extends Fragment implements Serializable, Post
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param postClicked POST
+     * @param postClicked     POST
      * @param commentListener CommentsInteractionListener
      * @return A new instance of fragment PostCommentsFragment.
      */
@@ -90,18 +92,18 @@ public class PostCommentsFragment extends Fragment implements Serializable, Post
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
-        if (mView == null){
+        if (mView == null) {
             // Inflate the layout for this fragment
             mView = inflater.inflate(R.layout.fragment_post_comments, container, false);
 
             mRecyclerView = (RecyclerView) mView.findViewById(R.id.comments_recycler_view);
             mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-            mLoading = (RelativeLayout)mView.findViewById(R.id.comments_progressbar_layout);
+            mLoading = (RelativeLayout) mView.findViewById(R.id.comments_progressbar_layout);
             mComments = new ArrayList<>();
             mCommentsAdapter = new PostCommentsAdapter(getActivity(), mComments, PostCommentsFragment.this);
             mRecyclerView.setAdapter(mCommentsAdapter);
-            loadComments(mLoading, 0, 10);
+            loadComments(false, mLoading, 0, 10);
 
             mFab = (ImageButton) mView.findViewById(R.id.comments_fab);
             mFab.setOnClickListener(new View.OnClickListener() {
@@ -138,11 +140,11 @@ public class PostCommentsFragment extends Fragment implements Serializable, Post
                                         public void onClick(DialogInterface dialog, int id) {
                                             if (userInput.getText().toString().equals(""))
                                                 Toast.makeText(getActivity(), R.string.empty_comment_message, Toast.LENGTH_SHORT).show();
-                                            else{
+                                            else {
                                                 CommentsDataSource.postCommentAsUserOnPost(mLoading, getActivity(), mPost.getmObjectId(), userInput.getText().toString(), new FunctionCallback<Comment>() {
                                                     @Override
                                                     public void done(Comment comment, ParseException e) {
-                                                        if (e == null){
+                                                        if (e == null) {
                                                             mComments.add(0, comment);
                                                             mCommentsAdapter.notifyDataSetChanged();
                                                         } else {
@@ -173,7 +175,7 @@ public class PostCommentsFragment extends Fragment implements Serializable, Post
             Button shareButton = (Button) mView.findViewById(R.id.post_share_button);
             final String sharingPostText = getActivity().getResources().getString(R.string.sharing_post);
 
-            mRefreshComments = (SwipeRefreshLayout)mView.findViewById(R.id.comments_refresher);
+            mRefreshComments = (SwipyRefreshLayout) mView.findViewById(R.id.comments_refresher);
             mRefreshComments.setColorSchemeResources(R.color.accent, R.color.primary);
             mRefreshComments.setOnRefreshListener(this);
         }
@@ -181,24 +183,32 @@ public class PostCommentsFragment extends Fragment implements Serializable, Post
         return mView;
     }
 
-    private void loadComments (RelativeLayout loading, int startIndex, int numberOfComments){
+    private void loadComments(final boolean loadingMoreComments, RelativeLayout loading, int startIndex, int numberOfComments) {
         CommentsDataSource.getRangeOfCommentsForPost(loading, getActivity(), startIndex, numberOfComments, mPost.getmObjectId(), new FunctionCallback<ArrayList<Comment>>() {
             @Override
             public void done(ArrayList<Comment> comments, ParseException e) {
-                if (e == null){
+                mRefreshComments.setRefreshing(false);
+                if (e == null) {
                     Log.wtf("PostCommentsFragment", "comments loaded");
                     //instantiate and set the adapter
-                    mComments.clear();
+                    if (!loadingMoreComments)
+                        mComments.clear();
                     mComments.addAll(comments);
                     mCommentsAdapter.notifyDataSetChanged();
-                    //the animation for the recycler view to slide in from the bottom of the view
-                    TranslateAnimation trans = new TranslateAnimation(0, 0, 1000, 0);
-                    trans.setDuration(500);
-                    trans.setInterpolator(new DecelerateInterpolator(1.0f));
-                    mRecyclerView.startAnimation(trans);
-                }
-                else{
-                    Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
+                    if (!loadingMoreComments) {
+                        //the animation for the recycler view to slide in from the bottom of the view
+                        TranslateAnimation trans = new TranslateAnimation(0, 0, 1000, 0);
+                        trans.setDuration(500);
+                        trans.setInterpolator(new DecelerateInterpolator(1.0f));
+                        mRecyclerView.startAnimation(trans);
+                    }
+
+                } else {
+                    if (e.getCode() == ParseException.INCORRECT_TYPE)
+                        Toast.makeText(getActivity(), R.string.no_more_comments_message, Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
+
                     e.printStackTrace();
                 }
             }
@@ -216,12 +226,16 @@ public class PostCommentsFragment extends Fragment implements Serializable, Post
     }
 
     @Override
-    public void onRefresh() {
-        loadComments(mLoading, 0, 10);
+    public void onRefresh(SwipyRefreshLayoutDirection swipyRefreshLayoutDirection) {
+        if (swipyRefreshLayoutDirection == SwipyRefreshLayoutDirection.TOP)
+            loadComments(false, mLoading, 0, 10);
+        else
+            loadComments(true, mLoading, mComments.size(), 10);
     }
 
-    public interface CommentsInteractionListener  extends Serializable{
+    public interface CommentsInteractionListener extends Serializable {
         void pressedBackToPosts();
+
         void pressedCommenterProfile(User commenterPressed);
     }
 
